cc90c17bb1906958bdb37603464214b0aed2a9b0
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-09-10 22:57:23 +0900

    add : test code

--- src/test/emulator_test.cpp
+++ src/test/emulator_test.cpp
@@ -11,6 +11,7 @@ class FIXTURE_NAME : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(FIXTURE_NAME);
     CPPUNIT_TEST(test_mov_r32_imm32);
     CPPUNIT_TEST(test_near_jump);
+    CPPUNIT_TEST(test_modrm);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -19,6 +20,7 @@ public:
 protected:
     void test_mov_r32_imm32();
     void test_near_jump();
+    void test_modrm();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(FIXTURE_NAME);
@@ -76,3 +78,32 @@ void FIXTURE_NAME::test_near_jump(){
     CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[ESI]);
     CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDI]);
 }
+
+void FIXTURE_NAME::test_modrm(){
+    emulator emu(1024 * 1024, 0x7c00, 0x7c00);
+    
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EAX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[ECX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EBX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x007c00, emu.registers[ESP]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EBP]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[ESI]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDI]);
+    
+    emu.load_program("bin/data/modrm-test.bin", 0x0200, 0x7c00);
+    while(emu.exec()){
+        printf("!\n");
+    }
+    
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000002, emu.registers[EAX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[ECX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EBX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x007bf0, emu.registers[ESP]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x007bf0, emu.registers[EBP]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000008, emu.registers[ESI]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDI]);
+}
+
+
