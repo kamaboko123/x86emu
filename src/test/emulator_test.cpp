5274d0e5153abc1c9833583ec4a25fffead49146
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-09-21 22:36:03 +0900

    add: test code for call

--- src/test/emulator_test.cpp
+++ src/test/emulator_test.cpp
@@ -11,14 +11,17 @@ class FIXTURE_NAME : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(FIXTURE_NAME);
     CPPUNIT_TEST(test_near_jump);
     CPPUNIT_TEST(test_modrm);
+    CPPUNIT_TEST(test_call);
     CPPUNIT_TEST_SUITE_END();
 
 public:
     void setUp();
     void tearDown();
 protected:
+    void test_init();
     void test_near_jump();
     void test_modrm();
+    void test_call();;
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(FIXTURE_NAME);
@@ -27,7 +30,7 @@ void FIXTURE_NAME::setUp() {}
 
 void FIXTURE_NAME::tearDown() {}
 
-void FIXTURE_NAME::test_near_jump(){
+void FIXTURE_NAME::test_init(){
     emulator emu(1024 * 1024, 0x7c00, 0x7c00);
     
     CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EAX]);
@@ -38,7 +41,10 @@ void FIXTURE_NAME::test_near_jump(){
     CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EBP]);
     CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[ESI]);
     CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDI]);
-    
+}
+
+void FIXTURE_NAME::test_near_jump(){
+    emulator emu(1024 * 1024, 0x7c00, 0x7c00);
     emu.load_program("bin/data/near_jump.bin", 0x0200);
     while(emu.exec());
     
@@ -54,16 +60,6 @@ void FIXTURE_NAME::test_near_jump(){
 
 void FIXTURE_NAME::test_modrm(){
     emulator emu(1024 * 1024, 0x7c00, 0x7c00);
-    
-    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EAX]);
-    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[ECX]);
-    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDX]);
-    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EBX]);
-    CPPUNIT_ASSERT_EQUAL((uint32_t)0x007c00, emu.registers[ESP]);
-    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EBP]);
-    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[ESI]);
-    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDI]);
-    
     emu.load_program("bin/data/modrm-test.bin", 0x0200);
     while(emu.exec());
     
@@ -77,4 +73,18 @@ void FIXTURE_NAME::test_modrm(){
     CPPUNIT_ASSERT_EQUAL((uint32_t)0x000008, emu.registers[EDI]);
 }
 
+void FIXTURE_NAME::test_call(){
+    emulator emu(1024 * 1024, 0x7c00, 0x7c00);
+    emu.load_program("bin/data/call-test.bin", 0x0200);
+    while(emu.exec());
+    
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x0000f1, emu.registers[EAX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x00011a, emu.registers[ECX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000029, emu.registers[EBX]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x007c00, emu.registers[ESP]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x007000, emu.registers[EBP]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[ESI]);
+    CPPUNIT_ASSERT_EQUAL((uint32_t)0x000000, emu.registers[EDI]);
+}
 
